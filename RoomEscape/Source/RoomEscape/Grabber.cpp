b2a6337c29cfa8dd610bf3d4295046ad9200ac6b
Kobalty <kobalty@outlook.com>
2017-05-05 22:34:42 +0100

    got round to finding view controler not shit wont compile :(

--- RoomEscape/Source/RoomEscape/Grabber.cpp
+++ RoomEscape/Source/RoomEscape/Grabber.cpp
@@ -20,6 +20,17 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
+	FVector FirstPlayerLocation;// Empty vector
+	FRotator FirstPlayerRotation;// Empty Rotator
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(FirstPlayerLocation,FirstPlayerRotation);// this allows me to get a return of location and rotation need to find something to sotre it in tho.
+
+	FString FirstPlayerLocationLog = FirstPlayerLocation.ToString();// allows the FPLocation to be outputted as a string
+	FString FirstPlayerRotationLog = FirstPlayerRotation.ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("Player Location is %s"), *FirstPlayerLocationLog);
+	UE_LOG(LogTemp, Warning, TEXT("Player Rotation is %s"), *FirstPlayerRotationLog);
+
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting for duty..tolotaly not pervy :P")); // just lets me know the Grabber is ready to go. Putting a ";" causes this not to work.
 	
